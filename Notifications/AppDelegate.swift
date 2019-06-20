35a6fc8db8a9a1dfcd8dbe05feee4ab65fa7b885
Kostya Likhachev <arrow.koss@gmail.com>
2019-06-20 13:45:52 +0300

    - refactoring

--- Notifications/AppDelegate.swift
+++ Notifications/AppDelegate.swift
@@ -13,85 +13,16 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let notificationCenter = UNUserNotificationCenter.current()
-
+    let notifications = Notifications()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        requestAuthorization()
-        notificationCenter.delegate = self
+        notifications.requestAuthorization()
+        notifications.notificationCenter.delegate = notifications
         return true
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         UIApplication.shared.applicationIconBadgeNumber = 0
-    }
-    
-    func requestAuthorization() {
-        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
-            print("Permission granted: \(granted)")
-            guard granted else { return }
-            self.getNotificationSettings()
-        }
-    }
-    
-    func getNotificationSettings() {
-        notificationCenter.getNotificationSettings { (settings) in
-            print("Notitification settings: \(settings)")
-        }
-    }
-    
-    func scheduleNotitication(notificationType: String) {
-        let content = UNMutableNotificationContent()
-        let userAction = "User Action"
-        
-        content.title = notificationType
-        content.body = "This is example how to create" + notificationType
-        content.sound = UNNotificationSound.default
-        content.badge = 1
-        content.categoryIdentifier = userAction
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let identifier = "Local Notification"
-        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-        
-        notificationCenter.add(request) { (error) in
-            if let error = error {
-                print("Error \(error.localizedDescription)")
-            }
-        }
-        
-        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
-        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
-        let category = UNNotificationCategory(identifier: userAction, actions: [snoozeAction, deleteAction], intentIdentifiers: [], options: [])
-        
-        notificationCenter.setNotificationCategories([category])
-    }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .sound])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        if response.notification.request.identifier == "Local Notification" {
-            print("Handling notification wtih the local Ntification Identifier")
-        }
-        
-        switch response.actionIdentifier {
-        case UNNotificationDismissActionIdentifier:
-            print("Dismiss Action")
-        case UNNotificationDefaultActionIdentifier:
-            print("Defalut")
-        case "Snooze":
-            print("Snooze")
-            scheduleNotitication(notificationType: "Reminder")
-        case "Delete":
-            print("Delete")
-        default:
-            print("Unknown action")
-        }
-        
-        completionHandler()
     }
 }
 
